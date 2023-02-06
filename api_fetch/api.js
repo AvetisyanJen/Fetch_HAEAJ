86a5ccc60c67f9d481c5dd38caae28efcfc4c59f
Anna0345 <anna.serobyan97@gmail.com>
2023-02-06 12:20:24 +0400

    used map and filter on the first one instead of for of loop and for the second one used reduce method

--- api_fetch/api.js
+++ api_fetch/api.js
@@ -1,35 +1,53 @@
 // working with APIs
 
 async function getBooksAgatha() {
-  const response_first = await fetch("https://openlibrary.org/search.json?author=Agatha Christie");
-  const data_first = await response_first.json();
-  const books_agatha = data_first.docs.slice(0,50);
-  //books_agatha.sort((a, b) => new Date(a.first_publish_year) - new Date(b.first_publish_year))
-  books_agatha.sort((a, b) => a.title.localeCompare(b.title));
-  let filtered_data1= books_agatha.filter(author => author.title.length <= 40);
-  let list = "<ol>";
-  for (const book of filtered_data1) {
-    list += `<li>${book.title_suggest} (${book.language})</li>`;
+  try {
+    const response_first = await fetch("https://openlibrary.org/search.json?author=Agatha Christie");
+    const data = await response_first.json();
+    const books_agatha = data.docs
+      .slice(0,50)
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .filter(author => author.title.length <= 40);
+    
+    let list1 = "<ol>" + books_agatha.map(book => `<li>${book.title_suggest} (${book.language})</li>`).join("") + "</ol>";
+    
+    document.getElementById("second_author").innerHTML = list1;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("second_author").innerHTML = "An error occurred while fetching the books. Please try again later.";
   }
-  list += "</ol>";
-  document.getElementById("second_author").innerHTML = list;
 }
 getBooksAgatha()
+
+
+
+
 async function getDataTolkien() {
-  const response = await fetch("https://openlibrary.org/search.json?author=J.R.R. Tolkien");
-  const data = await response.json();
-  console.log(data);
-  const books = data.docs.slice(0,56);
-  books.sort((a, b) => new Date(a.first_publish_year) - new Date(b.first_publish_year))//returns the date number in increasing order
-  let filtered_data= books.filter(author => author.title.length <= 40);
-  let list = "<ol>";
-  for (const book of filtered_data) {
-    list += `<li>${book.title} (${book.first_publish_year})</li>`;
+  try {
+    const response = await fetch("https://openlibrary.org/search.json?author=J.R.R. Tolkien");
+    const data1 = await response.json();
+    const books_tolkien = data1.docs
+      .slice(0, 56)
+      .sort((a, b) => new Date(a.first_publish_year) - new Date(b.first_publish_year));
+    
+    let list2 = "<ol>" + books_tolkien.reduce((acc, book) => {
+      if (book.title.length <= 40) {
+        return acc + `<li>${book.title} (${book.first_publish_year})</li>`;
+      }
+      return acc;
+    }, "") + "</ol>";
+    
+    document.getElementById("books").innerHTML = list2;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("books").innerHTML = "An error occurred while fetching the books. Please try again later.";
   }
-  list += "</ol>";
-  document.getElementById("books").innerHTML = list;
 }
+
 getDataTolkien()
+
+
+
 
 
 
